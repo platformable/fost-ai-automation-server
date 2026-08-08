af08f79b3e7e520c58aceb110357805f94efe89a
Alexei Garban <garban.valdeon@gmail.com>
2026-08-08 21:23:46 +0200

    Refactor express middleware setup for improved readability

--- server.js
+++ server.js
@@ -27,8 +27,18 @@ setGlobalDispatcher(
 
 const app = express()
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(
+  express.json({
+    limit: "50mb",
+  }),
+)
+
+app.use(
+  express.urlencoded({
+    extended: true,
+    limit: "50mb",
+  }),
+)
 
 const PORT = process.env.PORT || 3000
 
